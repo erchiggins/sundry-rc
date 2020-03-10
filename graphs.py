5f62291540a6e3ed6cf9df1e48f93b4c0ccaf2cc
Emily Higgins <emilyrchiggins@gmail.com>
2020-03-10 17:02:26 -0400

    added bfs

--- graphs.py
+++ graphs.py
@@ -14,6 +14,35 @@ def dfs(node):
             dfs(neighbor)
 
 
+def bfs(node):
+    # store when each node was visited (at what 'level' it was seen)
+    level = {node: 0}
+    # track which node was visited directly before the current node
+    parent = {node: None}
+    i = 1
+    frontier = [node]
+    while frontier:
+        # store unvisited notes discovered in this iteration
+        next = []
+        # current group of nodes to explore
+        for u in frontier:
+            # check out neighbors of next node in frontier
+            for v in u.neighbors:
+                # have we seen this one yet?
+                if v not in level:
+                    level[v] = i
+                    parent[v] = u
+                    next.append(v)
+        frontier = next
+        i += 1
+    for l in level:
+        print(l.label, level[l])
+    for p in parent:
+        print(p.label)
+        if parent[p] is not None:
+            print(parent[p].label)
+
+
 node_a = GraphNode('a')
 node_b = GraphNode('b')
 node_c = GraphNode('c')
@@ -43,4 +72,5 @@ node_e.neighbors.add(node_d)
 
 graph = [node_a, node_b, node_c, node_d, node_e]
 
-dfs(node_a)
+bfs(node_a)
+
